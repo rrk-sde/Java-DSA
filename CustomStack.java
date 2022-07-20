559a06bb0c936cc2dfe7dcbf8999c2ccf5c9741b
rajeevrk9901 <rajeevrk9901@gmail.com>
2022-07-20 19:29:43 +0530

    Add files via upload

---
+++ CustomStack.java
@@ -0,0 +1,55 @@
+package com.rajeev;
+
+import java.sql.SQLOutput;
+
+public class CustomStack {
+
+    protected int[] data;
+    private static final int DEFAULT_SIZE =10;
+
+    int ptr =-1;
+
+    public CustomStack(){
+        this(DEFAULT_SIZE);
+    }
+
+    public CustomStack(int size){
+        this.data = new int[size];
+    }
+
+    public boolean push(int item){
+        if(isFull()){
+            System.out.println("Stack Is Full");
+            return false;
+        }
+
+        ptr++;
+        data[ptr]=item;
+        return true;
+    }
+
+    public int pop()  throws  StackException{
+        if(isEmpty()){
+            throw new StackException("cant pop from this empty stack");
+        }
+//        int removed = data[ptr];
+//        ptr--;
+//        return removed;
+        return data[ptr--];
+    }
+
+    public int peek() throws StackException{
+        if(isEmpty()){
+            throw new StackException("cant peek from this empty stack");
+        }
+        return data[ptr];
+    }
+
+    private boolean isFull(){
+        return ptr ==data.length-1;
+    }
+
+    private boolean isEmpty(){
+        return ptr ==-1;
+    }
+}
