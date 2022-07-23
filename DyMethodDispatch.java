4f59dff66319c8666ba877dd2c831ca6a737c5ca
rajeevrk9901 <rajeevrk9901@gmail.com>
2022-07-23 15:41:49 +0530

    Add files via upload

---
+++ DyMethodDispatch.java
@@ -0,0 +1,28 @@
+package com.rajeev;
+class Super{
+    void myth1(){
+        System.out.println("Super: Myth one");
+    }
+
+    void myth2(){
+        System.out.println("Super: Myth Two");
+    }
+}
+
+class Sub extends Super{
+    void myth2(){
+        System.out.println("sub: Myth 2");
+    }
+
+    void myth3(){
+        System.out.println("sub: Myth 3");
+    }
+}
+
+public class DyMethodDispatch {
+    public static void main(String[] args) {
+        Super t = new Sub();
+        t.myth1();
+        t.myth2();
+    }
+}
