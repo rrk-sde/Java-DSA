1a0b5673a17f5ebecec724506f379f116e63e9f2
Rajeev Ranjan Kumar <Rajeevrk9901@gmail.com>
2022-07-25 15:38:23 +0530

    Init Capacity and LoadFactor

--- HashSetDemo.java
+++ HashSetDemo.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 public class HashSetDemo {
 
     public static void main(String[] args) {
-        HashSet<Integer> hs = new HashSet<>();
+        HashSet<Integer> hs = new HashSet<>(20, 0.25F);
 
         hs.add(12);
         hs.add(20);
