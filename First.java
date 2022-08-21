87d9441e54a962ec988dab7f61719d7bd84c9611
Rajeev Ranjan Kumar <Rajeevrk9901@gmail.com>
2022-08-21 19:28:40 +0530

    ok

--- First.java
+++ First.java
@@ -1,10 +1,29 @@
-import java.applet.Applet;
-import java.awt.Graphics;
-
-public class First extends Applet {
-
-    public void paint(Graphics g) {
-        g.drawString("Welcome", 150, 150);
-
-    }
+class Super
+{
+Void meth1()
+{
+System.out.println(“meth1”);
+}
+Void meth2()
+{
+System.out.println(“super meth2”);
+}
+}
+class Sub extends Super
+{
+Void meth2()
+{
+System.out.println(“sub meth2”);
+}
+Void meth3()
+{
+System.out.println(“meth3”);
+}
+}
+class test
+{
+public.static.void.main()
+{
+Super s=new Sub();
+}
 }
